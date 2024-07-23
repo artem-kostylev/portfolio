67eab9ceb41bc6e79f1ea9d7509a784c4275b7bf
artem <kostylev.dev@gmail.com>
2024-07-23 17:29:45 -0500

    add more skills

--- src/components/home/skills.tsx
+++ src/components/home/skills.tsx
@@ -8,13 +8,15 @@ export const Skills = () => {
         <Badge variant="secondary">React</Badge>
         <Badge variant="secondary">Next.js</Badge>
         <Badge variant="secondary">TypeScript</Badge>
+        <Badge variant="secondary">JavaScript</Badge>
         <Badge variant="secondary">Node.js</Badge>
         <Badge variant="secondary">HTML</Badge>
         <Badge variant="secondary">CSS</Badge>
+        <Badge variant="secondary">Hono</Badge>
         <Badge variant="secondary">Shadcn/ui</Badge>
         <Badge variant="secondary">NestJS</Badge>
         <Badge variant="secondary">Vue</Badge>
-        <Badge variant="secondary">Nuxt</Badge>
+        <Badge variant="secondary">Nuxt.js</Badge>
         <Badge variant="secondary">TailwindCSS</Badge>
         <Badge variant="secondary">Postgres</Badge>
         <Badge variant="secondary">Drizzle</Badge>
