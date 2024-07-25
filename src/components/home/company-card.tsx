d034e5fa7fd8b4118c77d36f88bb3c457db73316
artem <kostylev.dev@gmail.com>
2024-07-24 20:59:40 -0500

    fix styles

--- src/components/home/company-card.tsx
+++ src/components/home/company-card.tsx
@@ -17,29 +17,19 @@ interface Props {
 export const CompanyCard = ({ company }: Props) => {
   return (
     <Card key={company.name}>
-      <CardHeader className="p-4">
-        <div className="flex items-center space-x-4">
-          <Avatar className="w-11 h-11">
-            <AvatarFallback>{company.name.charAt(0)}</AvatarFallback>
-            <AvatarImage src={company.image} asChild>
-              <Image
-                src={company.image}
-                alt={company.name}
-                className="w-full h-full"
-                sizes="44px"
-                fill
-              />
-            </AvatarImage>
-          </Avatar>
-          <div className="w-full">
-            <div className="flex items-center justify-between gap-2.5 mb-1">
-              <CardTitle className="text-lg leading-3">
-                {company.name}
-              </CardTitle>
-              <p className="text-sm text-muted-foreground">{company.period}</p>
-            </div>
-            <CardDescription>{company.position}</CardDescription>
+      <CardHeader className="p-4 items-center flex-row space-x-4">
+        <Avatar className="w-11 h-11">
+          <AvatarFallback>{company.name.charAt(0)}</AvatarFallback>
+          <AvatarImage src={company.image} asChild>
+            <Image src={company.image} alt={company.name} sizes="44px" fill />
+          </AvatarImage>
+        </Avatar>
+        <div className="flex-1">
+          <div className="flex items-center justify-between gap-2.5 mb-1">
+            <CardTitle className="text-lg leading-3">{company.name}</CardTitle>
+            <p className="text-sm text-muted-foreground">{company.period}</p>
           </div>
+          <CardDescription>{company.position}</CardDescription>
         </div>
       </CardHeader>
       <CardContent className="space-y-4">
