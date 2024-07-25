85c80593dc9df61015d3fa86e538ffdf2604618a
artem <kostylev.dev@gmail.com>
2024-07-24 20:09:52 -0500

    update about

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -3,10 +3,10 @@ export const About = () => {
     <section id="about">
       <h2 className="tracking-tight text-2xl font-bold mb-4">About</h2>
       <p className="text-lg text-muted-foreground">
-        I am a Software Engineer passionate about creating high-quality web
+        As a Software Engineer, I am passionate about creating high-quality web
         applications. I strive to build user-friendly and intuitive interfaces,
-        paying close attention to detail and performance. I continuously learn
-        and adapt to new technologies to provide the best solutions.
+        with a strong focus on detail and performance. I continuously learn and
+        adapt to new technologies to provide the best solutions.
       </p>
     </section>
   );
